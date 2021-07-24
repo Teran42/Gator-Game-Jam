ae99a7a630bfaa783f3c31dc6a05005c09ea846c
Teran42 <87882838+Teran42@users.noreply.github.com>
2021-07-24 12:13:41 -0400

    Add files via upload
    base script for cameracontroller and player controller

---
+++ PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            if (Input.GetKey(KeyCode.A))
+            {
+
+                rb.velocity = new Vector2(-5, rb.velocity.y);
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+
+                rb.velocity = new Vector2(5, rb.velocity.y);
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
+            }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        rb.velocity = new Vector2(rb.velocity.x, 10);
+
+        }
+
+        
+    }
+}
